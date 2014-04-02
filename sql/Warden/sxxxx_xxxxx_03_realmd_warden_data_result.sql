86e97f3ac51fd444bc5987c14eaeb5497884baa0
Salja <salja8840@gmail.com>
2014-04-02 21:09:44 +0200

    [WARDEN] Some Basic Classic MEM_CHECKS
    Signed-off-by: Salja <salja8840@gmail.com>

--- sql/Warden/sxxxx_xxxxx_03_realmd_warden_data_result.sql
+++ sql/Warden/sxxxx_xxxxx_03_realmd_warden_data_result.sql
@@ -1,5 +1,8 @@
 INSERT INTO `warden_data_result` (`id`,`check`, `data`, `str`, `address`, `length`, `result`, `comment`) VALUES
-(1,243,'','',4840352,2,'558B','lua protection'),
-(2,243,'','',8979979,5,'E04D62503F','Hyper speed'),
-(3,243,'','',9208923,5,'C0854A3340','gravity'),
-(4,243,'','',8100062,2,'7541','air jump');
+(1,217,'','SPEEDHACK-X86_64.DLL','','','','SPEED HACK - Injected DLL'),
+(2,217,'','WPESPY.DLL','','','','WPE PRO - Injected DLL'),
+(3,217,'','IHOOK.DLL','','','','ASM - Injected DLL'),
+(4,217,'','WHITEMAGIC.DLL','','','','Application Inject Library - Injected DLL'),
+(5,217,'','GREYMAGIC.DLL','','','','Automation Botting Library - Injected DLL'),
+(6,217,'','BLACKMAGIC.DLL','','','','Memory Inject Library - Injected DLL'),
+(7,217,'','RPE.DLL','','','','rEdoX Packet Editor - Injected DLL');
