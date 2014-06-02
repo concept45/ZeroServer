d3f7f5c61a7d940c295a2187ee7e1428ab27503e
Salja <salja8840@gmail.com>
2014-06-02 16:02:01 +0200

    Revert "Eluna Corepatch for https://github.com/ElunaLuaEngine/Eluna/commit/23ebd787b2a4812d055c9195f8050ad2272f0b43 Author Rochet"
    This reverts commit 5c7c67b83270b050f983fffd515dd00e04605a7d.
    
    Signed-off-by: Salja <salja8840@gmail.com>

--- src/game/Item.cpp
+++ src/game/Item.cpp
@@ -23,7 +23,7 @@
 #include "Database/DatabaseEnv.h"
 #include "ItemEnchantmentMgr.h"
 #include "SQLStorages.h"
-#include "LuaEngine.h"
+#include "HookMgr.h"
 
 void AddItemsSetItem(Player* player, Item* item)
 {
@@ -231,11 +231,6 @@ Item::Item() :
     m_lootState = ITEM_LOOT_NONE;
 }
 
-Item::~Item()
-{
-    Eluna::RemoveRef(this);
-}
-
 bool Item::Create(uint32 guidlow, uint32 itemid, Player const* owner)
 {
     Object::_Create(guidlow, 0, HIGHGUID_ITEM);
@@ -279,7 +274,7 @@ void Item::UpdateDuration(Player* owner, uint32 diff)
     if (GetUInt32Value(ITEM_FIELD_DURATION) <= diff)
     {
         // used by eluna
-        sEluna->OnExpire(owner, GetProto());
+        sHookMgr->OnExpire(owner, GetProto());
         owner->DestroyItem(GetBagSlot(), GetSlot(), true);
         return;
     }
